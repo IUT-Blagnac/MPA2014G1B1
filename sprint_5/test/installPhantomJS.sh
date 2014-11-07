ef6812a702837ac1c21455a073b91ebb5a130e83
Jeanjean Pierre <dark.phoenix.62@gmail.com>
2014-11-07 15:23:39 +0100

    Trying with sudo

--- sprint_5/test/installPhantomJS.sh
+++ sprint_5/test/installPhantomJS.sh
@@ -1,13 +1,13 @@
-apt-get remove phantomjs
+sudo apt-get remove phantomjs
 
-unlink /usr/local/bin/phantomjs
-unlink /usr/local/share/phantomjs
-unlink /usr/bin/phantomjs
+sudo unlink /usr/local/bin/phantomjs
+sudo unlink /usr/local/share/phantomjs
+sudo unlink /usr/bin/phantomjs
 
 cd /usr/local/share
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
-tar xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
+sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
+sudo tar xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
 
-ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs;
-ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs;
-ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/bin/phantomjs;
+sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs;
+sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs;
+sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/bin/phantomjs;
