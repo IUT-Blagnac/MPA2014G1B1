a783e2e9c95a06d20a07c8349b9a6afab30f86c5
Jeanjean Pierre <dark.phoenix.62@gmail.com>
2014-11-07 15:13:05 +0100

    Trying to fix the fix of the fix

--- sprint_5/test/installPhantomJS.sh
+++ sprint_5/test/installPhantomJS.sh
@@ -6,7 +6,7 @@ sudo unlink /usr/bin/phantomjs
 
 cd /usr/local/share
 sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
-tar xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
+sudo tar xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
 
 sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs;
 sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs;
